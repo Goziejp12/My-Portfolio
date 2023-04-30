b6772a1c427f6d9d64cec43e47efb26b10c3987e
Chigozie Johnpaul Maduka <chigoziemaduka401@gmail.com>
2023-04-30 15:22:11 +0100

    fix

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function HeroSection() {
+    console.log(window.innerWidth)
     return (
         <section className='hero--section'>
             <h1>frontend<span> Engineer</span></h1>
